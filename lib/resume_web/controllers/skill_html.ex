a4597f699067cafbdbd259a7525c40d36215555a
AVR-GCC <ogoun.d@gmail.com>
2025-10-09 16:17:47 +0300

    Add categories map to form attr

--- lib/resume_web/controllers/skill_html.ex
+++ lib/resume_web/controllers/skill_html.ex
@@ -12,6 +12,7 @@ defmodule ResumeWeb.SkillHTML do
   attr :changeset, Ecto.Changeset, required: true
   attr :action, :string, required: true
   attr :return_to, :string, default: nil
+  attr :categories, :map, default: %{}
 
   def skill_form(assigns)
 end
